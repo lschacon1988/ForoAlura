97997665fbdbaf7a7ffdd603686e683ddf20598e
lschacon1988 <lschacon1988@gmail.com>
2023-09-20 12:10:42 -0500

    fix(config) update config

--- src/main/java/com/alura/foro/APIRest/ApiRestApplication.java
+++ src/main/java/com/alura/foro/APIRest/ApiRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiRestApplication {
-
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
