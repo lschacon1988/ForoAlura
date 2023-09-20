dfd2b916fd7e73f47a7cb7a35a31482702cb43e3
lschacon1988 <lschacon1988@gmail.com>
2023-09-20 17:32:56 -0500

    fix(config) update cors

--- src/main/java/com/alura/foro/APIRest/config/SwaggerConfigure.java
+++ src/main/java/com/alura/foro/APIRest/config/SwaggerConfigure.java
@@ -2,11 +2,15 @@ package com.alura.foro.APIRest.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfigure {
@@ -21,6 +25,19 @@ public class SwaggerConfigure {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(
+                        RequestHandlerSelectors
+                                .basePackage("src/main/java/com/alura/foro/APIRest/controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
 
     @Bean
     public void message(){
