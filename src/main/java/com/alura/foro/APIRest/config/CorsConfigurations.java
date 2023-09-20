9796310993b5e5cc740ed4f90d4f44b2b57ff844
lschacon1988 <lschacon1988@gmail.com>
2023-09-20 16:12:05 -0500

    fix(config) update cors

--- src/main/java/com/alura/foro/APIRest/config/CorsConfigurations.java
+++ src/main/java/com/alura/foro/APIRest/config/CorsConfigurations.java
@@ -10,13 +10,12 @@ public class CorsConfigurations implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-<<<<<<< HEAD
+
                 .allowedOrigins(
                         "https://foroalura-production.up.railway.app/**",
                         "http://foroalura-production.up.railway.app/**")
-=======
-                .allowedOrigins("http://foroalura-production.up.railway.app/**")
->>>>>>> e162cbeb6dad58dd31d38a54635657bbaa42b338
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE",
+                        "CONNECT");
     }
 }
